743f2ac3cd61ae3e96fcbd5f4c343f48e15cdf82
gbortolini9 <119908312+gbortolini9@users.noreply.github.com>
2023-11-07 14:02:00 -0300

    muitas atualizações

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -16,6 +16,25 @@ function toggleMenu(event) {
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
 
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 7000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 5){
+        cont = 1 
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}
+
 window.sr = ScrollReveal({ reset: true});
 
 sr.reveal('.topico-01', {duration: 2000});
